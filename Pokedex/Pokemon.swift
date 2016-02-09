d07854b1eb5eb1561e53954fbebd8fe7943b84ee
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-09 21:34:13 +0900

    Made Pokemon class

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Stanley Pan on 2/9/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
