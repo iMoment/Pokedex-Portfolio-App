26b227f89685e3f0834331a8326c20bb7917fb1a
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-11 15:21:56 +0900

    Integrate Alamofire, add Constants swift file

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Stanley Pan on 2/11/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
